f48321d79fa17c62438944651ca27e004bc28359
KingPra <kingi.pra@gmail.com>
2017-04-06 17:37:45 -0400

    fixed instagram link

--- gulpfile.js
+++ gulpfile.js
@@ -6,20 +6,21 @@
   */
 
   // Step 1: import gulp
-  let gulp = require('gulp');
-  let sass = require('gulp-sass');
-  let browser = require('gulp-browser');
+  const gulp = require('gulp');
+  const sass = require('gulp-sass');
+  const browser = require('gulp-browser');
+  const imagemin = require('gulp-imagemin')
 
   // Step 2: create default tasks
-  gulp.task('default', ['html', 'css', 'js']);
+  gulp.task('default', ['html', 'css', 'js', 'images']);
 
   // Step 3: create subtasks
-  gulp.task('html', function () {
+  gulp.task('html', () => {
       return gulp.src('index.html')
         .pipe(gulp.dest('docs/'));
   });
 
-  gulp.task('css', function () {
+  gulp.task('css', () => {
       // Convert style.scss into style.css
       // Copy to docs/ 
       return gulp.src('scss/style.scss')
@@ -27,15 +28,22 @@
         .pipe(gulp.dest('docs/'));
   });
 
-  gulp.task('js', function () {
+  gulp.task('js', () => {
       // Copy js file into docs/
       return gulp.src('js/app.js')
         .pipe(browser.browserify()) // makes require work
         .pipe(gulp.dest('docs/'));
   });
 
-  gulp.task('watch', ['default'], function () {
-   gulp.watch('*.html',['html']); //watches all html files
-   gulp.watch('scss/*.scss', ['css']);
-   gulp.watch('js/*/*.js', ['js']);
+  gulp.task('images', () => {
+    return gulp.src('images/*')
+      .pipe(imagemin())
+      .pipe(gulp.dest('docs/images'))
+  });
+
+  gulp.task('watch', ['default'], () => {
+  gulp.watch('*.html',['html']); //watches all html files
+  gulp.watch('scss/*.scss', ['css']);
+  gulp.watch('js/*/*.js', ['js']);
+  gulp.watch('images/*', ['images']);
 });
