b78678411ab7499e32498056c65175a97ddefc3d
KingPra <kingi.pra@gmail.com>
2017-04-07 13:34:37 -0400

    adding css grid

--- js/app.js
+++ js/app.js
@@ -14,25 +14,12 @@ app.config(function ($stateProvider) {
         url: '/dyno',
         component: 'dyno',
     });
-    // ...we can set up any number of states
-});
-
-// Each one of these represents a 'view' on the page.
-app.controller('HomeController', function () {
-});
-
-// $routeParams - parameters provided by the router
-// ^ provided by ui-router, have to use this name
-// $stateParams is used whenever we need to get information from the route 
-app.controller('DynoController', function () {
 });
 
 app.component('home', {
-    controller: 'HomeController',
     templateUrl: 'templates/home.html',
 });
 
 app.component('dyno', {
-    controller: 'DynoController',
     templateUrl: 'templates/dyno.html',
 });
