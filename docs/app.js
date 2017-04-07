966918a48f31fa7ec5c85795c6f0f7bfecec6e74
KingPra <kingi.pra@gmail.com>
2017-04-06 23:03:51 -0400

    app.js file cleanup

--- docs/app.js
+++ docs/app.js
@@ -26,11 +26,8 @@ app.controller('HomeController', function () {
 // ^ provided by ui-router, have to use this name
 // $stateParams is used whenever we need to get information from the route 
 app.controller('DynoController', function () {
-    // Why person_id? 
-    // Note: need to parseInt - these will be strings by default
 });
 
-/* Defining a component */
 app.component('home', {
     controller: 'HomeController',
     templateUrl: 'templates/home.html',
@@ -39,19 +36,6 @@ app.component('home', {
 app.component('dyno', {
     controller: 'DynoController',
     templateUrl: 'templates/dyno.html',
-});
-
-// app.component('friendSummary', {
-//     templateUrl: 'templates/summary.html',
-//     bindings: {
-//         person: '<',
-//         whenIClick: '&', // functions are &
-//     },
-// });
-
-app.factory('NuggetService', function () {
-    return {
-    };
 });
 
 },{}]},{},[1]);
